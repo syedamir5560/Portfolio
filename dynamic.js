aa33089adc4246190bd0c7bba9d20b06c37d4ca4
syedamir5560 <syedamir5560@gmail.com>
2023-11-26 23:12:41 +0530

    about

--- dynamic.js
+++ dynamic.js
@@ -1,6 +1,6 @@
 var typed = new Typed('.text',{
 strings:["Web Developer","Front-End Developer","JavaScript Developer","Mern-Stack Developer"],
-typeSpeed:100,
+typeSpeed:50,
 backSpeed:100,
 backDelay:1000,
 loop:true
